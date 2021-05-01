ab7af5bf01cde13d506af9bbc6f774c6db723523
Thomas Preston <thomas.diamond.dude@gmail.com>
2021-05-01 16:08:45 +0100

    Removed Temp Items & Started Calculator Project

---
+++ Temp/Calculator/Calculator.py
@@ -0,0 +1,84 @@
+# Author: Thomas Preston
+
+from tkinter import *
+
+class Calc():
+    window_width = 300
+    window_height = 500
+    window_x = 200
+    window_y = 100
+
+    background_colour = "#303030"
+
+    def main(self):
+        self.window = Tk()
+        self.window.geometry("{0}x{1}+{2}+{3}".format(self.window_width, self.window_height, self.window_x, self.window_y))
+        self.window.title("Calculator")
+        self.window.resizable(False, False)
+        self.window.configure(bg = self.background_colour)
+        self.window.iconbitmap("Calc.ico")
+
+    def draw(self):
+        placeholder = Label(self.window, text = " ", bg = self.background_colour, fg = self.background_colour)
+        placeholder.grid(row = 0, column = 0, rowspan = 10)
+
+        lblf = LabelFrame(self.window, bd = 5, relief = RIDGE, bg = "white", fg = "white")
+        lblf.grid(row = 2, column = 0, columnspan = 4)
+
+        self.lbl = Label(lblf, text = "", bg = "white", fg = "white")
+        self.lbl.pack()
+
+        placehold = Label(lblf, text = "                                                                  ", bg = "white", fg = "white")
+        placehold.pack()
+
+        placeholder2 = Label(self.window, text = " ", bg = self.background_colour, fg = self.background_colour)
+        placeholder2.grid(row = 2, column = 0, columnspan = 3)
+
+        # lblf2 = LabelFrame(self.window, bd = 0, bg = self.background_colour, fg = self.background_colour)
+        # lblf2.grid(row = 4, column = 0, rowspan = 4, columnspan = 2)
+        
+        row = 3
+        column = 2
+        columnspan = 1
+        width = 2
+        for i in range(0, 10):
+            if row == 6:
+                column = 0
+                columnspan = 2
+                width = 5
+            i = 9 - i
+            btn = Button(self.window, text = i, font = "arial 25", bd = 4, bg = self.background_colour, activebackground = self.background_colour, command = lambda num = i: self.math(num), relief = RIDGE, fg = "white", width = width)
+            btn.grid(row = row, column = column, columnspan = columnspan)
+            if column == 0:
+                column = 2
+                row += 1
+                continue
+            column -= 1
+
+        width = 2
+
+        btn = Button(self.window, text = ".", font = "arial 25", bd = 4, bg = self.background_colour, activebackground = self.background_colour, command = None, relief = RIDGE, fg = "white", width = width)
+        btn.grid(row = 6, column = 2)
+        # lblf3 = LabelFrame(self.window, bd = 0, bg = self.background_colour, fg = self.background_colour)
+        # lblf3.grid(row = 4, column = 2, rowspan = 5)
+
+        row = 3
+        column = 3
+        for i in ["÷", "×", "-", "+", "="]:
+            btn = Button(self.window, text = i, font = "arial 25", bd = 4, bg = self.background_colour, activebackground = self.background_colour, command = None, relief = RIDGE, fg = "white", width = width)
+            btn.grid(row = row, column = column)
+            row += 1
+    
+    def math(self, num):
+        pass
+
+
+    def run(self):
+        self.main()
+
+        self.draw()
+
+        self.window.mainloop()
+
+ui = Calc()
+ui.run()
